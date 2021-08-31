5ce6be5688c7234f80cbda5880aa1a6f5b4a5c4d
Meysam Mashhadi <48321419+MeMashhadi@users.noreply.github.com>
2021-08-31 12:11:38 +0200

    Add files via upload
    This project is in Visual C#

---
+++ Threebuttons/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ThreeButtons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnTerminate = new System.Windows.Forms.Button();
+            this.btnNewInstance = new System.Windows.Forms.Button();
+            this.btnCloseAll = new System.Windows.Forms.Button();
+            this.NewRunTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnTerminate
+            // 
+            this.btnTerminate.Location = new System.Drawing.Point(12, 134);
+            this.btnTerminate.Name = "btnTerminate";
+            this.btnTerminate.Size = new System.Drawing.Size(75, 23);
+            this.btnTerminate.TabIndex = 0;
+            this.btnTerminate.Text = "Terminate";
+            this.btnTerminate.UseVisualStyleBackColor = true;
+            this.btnTerminate.Click += new System.EventHandler(this.btnTerminate_Click);
+            // 
+            // btnNewInstance
+            // 
+            this.btnNewInstance.Location = new System.Drawing.Point(93, 134);
+            this.btnNewInstance.Name = "btnNewInstance";
+            this.btnNewInstance.Size = new System.Drawing.Size(97, 23);
+            this.btnNewInstance.TabIndex = 1;
+            this.btnNewInstance.Text = "New Instance";
+            this.btnNewInstance.UseVisualStyleBackColor = true;
+            this.btnNewInstance.Click += new System.EventHandler(this.btnNewInstance_Click);
+            // 
+            // btnCloseAll
+            // 
+            this.btnCloseAll.Location = new System.Drawing.Point(196, 134);
+            this.btnCloseAll.Name = "btnCloseAll";
+            this.btnCloseAll.Size = new System.Drawing.Size(75, 23);
+            this.btnCloseAll.TabIndex = 2;
+            this.btnCloseAll.Text = "Close All";
+            this.btnCloseAll.UseVisualStyleBackColor = true;
+            this.btnCloseAll.Click += new System.EventHandler(this.btnCloseAll_Click);
+            // 
+            // NewRunTimer
+            // 
+            this.NewRunTimer.Tick += new System.EventHandler(this.NewRunTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(278, 166);
+            this.Controls.Add(this.btnCloseAll);
+            this.Controls.Add(this.btnNewInstance);
+            this.Controls.Add(this.btnTerminate);
+            this.Name = "Form1";
+            this.Text = "Main";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTerminate;
+        private System.Windows.Forms.Button btnNewInstance;
+        private System.Windows.Forms.Button btnCloseAll;
+        private System.Windows.Forms.Timer NewRunTimer;
+    }
+}
+
---
+++ Threebuttons/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace ThreeButtons
+{
+    public partial class Form1 : Form
+    {
+        string KeyAddress = "HKEY_CURRENT_USER\\Software\\NordsonTest";
+
+        public Form1()
+        {
+            RegistryKey SubKey = Registry.CurrentUser.OpenSubKey("Software\\NordsonTest", false);
+            // if the subkey is null means it's the first time to run this program on this machine.
+            // so need to create subkey and key value in the registry.
+            if (SubKey == null)
+            {
+                Registry.CurrentUser.CreateSubKey("Software\\NordsonTest");
+                Registry.SetValue(KeyAddress, "NewExecution", "Yes");
+            }
+
+            // If new execution accrues, it changes the key value in the registry and the main form checks this key value
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                Registry.SetValue(KeyAddress, "NewExecution", "Yes");
+                Environment.Exit(0);
+                Application.Exit();
+            }
+            InitializeComponent();
+            NewRunTimer.Enabled = true;
+        }
+
+        private void btnTerminate_Click(object sender, EventArgs e)
+        {
+            // hide the main Form or terminate other forms 
+            if (this.Text == "Main")
+            {
+                this.Visible = false;
+            }
+            else
+            {
+                Application.ExitThread();
+            }
+        }
+
+        private void btnNewInstance_Click(object sender, EventArgs e)
+        {
+            // starta new instance of this form with title of Instance
+            new Thread(() => {
+                Form1 NewForm = new Form1();
+                NewForm.Text = "Instance";
+                Application.Run(NewForm);
+
+            }).Start();
+        }
+
+        private void btnCloseAll_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void NewRunTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.Text == "Main")
+            {
+                if (Registry.GetValue(KeyAddress, "NewExecution", "No").ToString() == "Yes")
+                {
+                    // New execution changes "NewExecution" key in registery and here we know that new execution happened. 
+                    // then restore the main form and bring it to the top of Z-order
+                    Registry.SetValue(KeyAddress, "NewExecution", "No");
+                    this.Visible = true;
+                    this.Activate();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.Text == "Main")
+            {
+                Visible = false;
+                // Cancel the Closing event from closing the Main form.
+                e.Cancel = true; 
+            }
+        }
+    }
+}
